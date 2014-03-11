086868c27eafcf1437cd4694b5c9651c04b4db72
dsofer <d@sofer.com>
2014-03-11 14:06:17 +0000

    install.sh edit

--- install.sh
+++ install.sh
@@ -9,12 +9,12 @@ echo sudo apt-get install -y openjdk-7-jdk
 sudo apt-get install -y openjdk-7-jdk
 echo mkdir "~/bin"
 mkdir ~/bin
+echo . .profile (run .profile to add new bin directory to path)
+. .profile
 echo wget -O ~/bin/lein https://raw.github.com/technomancy/leiningen/stable/bin/lein
 wget -O ~/bin/lein https://raw.github.com/technomancy/leiningen/stable/bin/lein
 echo "chmod 755 ~/bin/lein"
 chmod 755 ~/bin/lein
-echo "echo export PATH='$PATH':~/bin >> ~/.profile"
-echo export PATH='$PATH':~/bin >> ~/.profile
 echo git clone https://github.com/LightTable/LightTable.git
 git clone https://github.com/LightTable/LightTable.git
 echo cd LightTable
