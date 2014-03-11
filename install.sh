4fa80fc3979cb99204e04dbb98cbabb7467155cb
dsofer <d@sofer.com>
2014-03-11 13:45:44 +0000

    install.sh edit

--- install.sh
+++ install.sh
@@ -1,10 +1,22 @@
+echo 'cd $HOME'
 cd $HOME
-sudo apt-get install -y git
-sudo apt-get install -y curl
-mkdir ~/bin
-wget -O ~/bin/lein https://raw.github.com/technomancy/leiningen/stable/bin/lein
-chmod 755 ~/bin/lein
-echo export PATH='$PATH':~/bin >> ~/.profile
-git clone https://github.com/LightTable/LightTable.git
-cd LightTable
-bash linux_deps.sh
+echo sudo apt-get install -y git
+#sudo apt-get install -y git
+echo sudo apt-get install -y curl
+#sudo apt-get install -y curl
+echo sudo apt-get install -y openjdk-7-jdk
+#sudo apt-get install -y openjdk-7-jdk
+echo mkdir ~/bin
+#mkdir ~/bin
+echo wget -O ~/bin/lein https://raw.github.com/technomancy/leiningen/stable/bin/lein
+#wget -O ~/bin/lein https://raw.github.com/technomancy/leiningen/stable/bin/lein
+echo chmod 755 ~/bin/lein
+#chmod 755 ~/bin/lein
+echo "echo export PATH='$PATH':~/bin >> ~/.profile"
+#echo export PATH='$PATH':~/bin >> ~/.profile
+echo git clone https://github.com/LightTable/LightTable.git
+#git clone https://github.com/LightTable/LightTable.git
+echo cd LightTable
+#cd LightTable
+echo bash linux_deps.sh
+#bash linux_deps.sh
