fadb546e55dc986b3454fb95779a1f5cbdd1d29b
dsofer <d@sofer.com>
2014-03-11 12:17:04 +0000

    install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ sudo apt-get install -y curl
 mkdir ~/bin
 wget -O ~/bin/lein https://raw.github.com/technomancy/leiningen/stable/bin/lein
 chmod 755 ~/bin/lein
-export PATH=$PATH:~/bin
+echo export PATH='$PATH':~/bin >> ~/.profile
 git clone https://github.com/LightTable/LightTable.git
 cd LightTable
 bash linux_deps.sh
