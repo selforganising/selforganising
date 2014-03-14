051731337ffe657b6eaa080f55a581c22e029c80
dsofer <d@sofer.com>
2014-03-14 13:04:58 +0000

    install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 echo
 echo 'Change to home directory: cd $HOME'
 cd $HOME
-echo 'Dowload git: sudo apt-get install -y git' 
+echo 'Install git: sudo apt-get install -y git' 
 sudo apt-get install -y git
-echo 'Download curl: sudo apt-get install -y curl'
+echo 'Install curl: sudo apt-get install -y curl'
 sudo apt-get install -y curl
-echo 'Dowload java: sudo apt-get install -y openjdk-7-jdk'
+echo 'Install chromium: sudo apt-get install -y chromium-browser'
+sudo apt-get install -y chromium-browser
+echo 'Install java: sudo apt-get install -y openjdk-7-jdk'
 sudo apt-get install -y openjdk-7-jdk
 echo 'Add a bin directory: mkdir ~/bin'
 mkdir ~/bin
